3765c33fdb88d3a15b32c3c70753546ecd3ac3f4
Andrew Kyanjo <59293622+AndrewKyanjo@users.noreply.github.com>
2026-06-12 14:03:16 +0300

    feat: initialize Next.js project with Tailwind CSS, Supabase, and TypeScript
    - Add package.json with dependencies and scripts for Next.js, React, and Tailwind CSS.
    - Create postcss.config.mjs for Tailwind CSS integration.
    - Add SVG icons: file.svg, globe.svg, next.svg to public directory.
    - Include favicon.ico in the app.
    - Create global styles in globals.css with Tailwind CSS.
    - Set up layout component in layout.tsx with custom fonts and metadata.
    - Implement home page in page.tsx with a greeting and an image.
    - Configure TypeScript settings in tsconfig.json for Next.js.

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Image
+        src="/globe.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+    </div>
+  );
+}
